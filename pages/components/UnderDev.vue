75fb98d8a6f697a7d510d572ea3c277e1ba68c8e
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-11-13 17:04:25 +0530

    Create UnderDev.vue

---
+++ pages/components/UnderDev.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex items-center justify-center h-screen">
+        <div class="flex flex-col items-center w-fit px-10 py-5 shadow-2xl rounded-xl">
+            <span class="font-thin sm:text-3xl md:text-4xl lg:max-w-2xl xl:max-w-4xl text-center text-white select-none">
+                Under Development
+            </span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+useHead({
+    title: 'Worker | Under Dev'
+})
+</script>
+
+<style scoped>
+.wierd-corners {
+    border-radius: 0.75rem 0 0.75rem 0;
+}
+</style>
