37287671e6682a105885673ecf8e36288ee4da8a
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-11-13 17:04:33 +0530

    Update nuxt.config.ts

--- pages/nuxt.config.ts
+++ pages/nuxt.config.ts
@@ -1,7 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: false },
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ]
+	ssr: true,
+	devtools: { enabled: false },
+	modules: ['@nuxtjs/tailwindcss', "@nuxt/image"],
+	app: {
+		head: {
+			title: 'Animepahe Downloader',
+		}
+	},
+	runtimeConfig: {
+		public: {
+			DEV: false,
+			API: 'https://anime.disnakamadushan66.workers.dev/',
+		}
+	},
+	image: {
+		dir: 'assets'
+	},
+	nitro: {
+		prerender: {
+			routes: [
+				'/_ipx/h_205&f_png&blur_10&q_50/pahe.png',
+				'/_ipx/h_384&f_png&blur_10&q_50/pahe.png'
+			]
+		}
+	}
 })
