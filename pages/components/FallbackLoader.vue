aa0dd67f0231d424ab951c0a42ff40a004f75efc
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-11-13 17:04:23 +0530

    Create FallbackLoader.vue

---
+++ pages/components/FallbackLoader.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex justify-center items-center h-screen">
+        <span class="loading loading-infinity loading-lg text-primary"></span>
+    </div>
+</template>
+
+<script setup>
+useHead({
+    title: 'Page | Loading...'
+})
+</script>
