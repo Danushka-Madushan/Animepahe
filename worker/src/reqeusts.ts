b7278fca96f9bedaf5d40825fb3843f86c4cf5d3
Danushka-Madushan <Danushkamadushan128@gmail.com>
2024-02-10 07:13:29 +0530

    bypass ddosguard

--- worker/src/reqeusts.ts
+++ worker/src/reqeusts.ts
@@ -32,8 +32,18 @@ export class AnimePahe {
 
 	private static Headers(streamUrl: string | false, userAgent: string) {
 		return {
+			'authority': 'animepahe.ru',
 			'accept': 'application/json, text/javascript, */*; q=0.01',
-			'cookie': 'cf_clearance=Origin;',
+			'accept-language': 'en-US,en;q=0.9',
+			'cookie': '__ddg2_=;',
+			'dnt': '1',
+			'sec-ch-ua': '"Not A(Brand";v="99", "Microsoft Edge";v="121", "Chromium";v="121"',
+			'sec-ch-ua-mobile': '?0',
+			'sec-ch-ua-platform': '"Windows"',
+			'sec-fetch-dest': 'empty',
+			'sec-fetch-mode': 'cors',
+			'sec-fetch-site': 'same-origin',
+			'x-requested-with': 'XMLHttpRequest',
 			'referer': streamUrl ? `https://animepahe.ru/anime/${ streamUrl }` : 'https://animepahe.ru',
 			'user-agent': userAgent,
 		}
