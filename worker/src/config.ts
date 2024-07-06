fb923dc320c11f62f98c62814a7b7fbd7aa1ea97
Danushka Madushan <69332746+Danushka-Madushan@users.noreply.github.com>
2024-07-06 20:10:43 +0530

    update config.ts

--- worker/src/config.ts
+++ worker/src/config.ts
@@ -3,9 +3,10 @@ export interface Env {
 }
 
 export const CORS = {
-	"Access-Control-Allow-Origin": '*',
-	"Access-Control-Allow-Methods": "GET,POST,HEAD,OPTIONS",
-	"Access-Control-Max-Age": "86400",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, HEAD, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Max-Age": "86400",
 }
 
 export const WorkerResponse = (body: object | string, type: "application/json" | "text/html;charset=UTF-8") => {
