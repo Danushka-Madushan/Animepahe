a94b82f39dcf186aca0b0cdef57bfbf3ba5ec897
Danushka Madushan <69332746+Danushka-Madushan@users.noreply.github.com>
2024-07-06 23:29:47 +0530

    feat: create SearchResultItem.tsx

---
+++ pages/src/components/SearchResultItem.tsx
@@ -0,0 +1,74 @@
+import { Card, CardHeader, CardBody, Image, Divider, Chip, Spinner } from '@nextui-org/react'
+import { Prox } from '../utils/ImgProxy'
+import useAxios from '../hooks/useAxios'
+import { ANIME } from '../config/config'
+import { EpisodeResult } from 'fetch/requests'
+import { Dispatch, SetStateAction } from 'react'
+
+interface SearchResultItemProps {
+  title: string,
+  episodes: number,
+  poster: string,
+  status: string,
+  type: string,
+  year: number,
+  session: string,
+  score: number | null,
+  setEpisodes: Dispatch<SetStateAction<EpisodeResult['episodes']>>
+  setBreadcrumbs: (title: string) => void,
+  setSelectedSeriesID: Dispatch<SetStateAction<string>>,
+  setPagination: Dispatch<SetStateAction<number>>,
+}
+
+const SearchResultItem = ({ 
+  session, title, episodes, poster, status, type, year, score,
+  setBreadcrumbs, setEpisodes, setSelectedSeriesID, setPagination
+}: SearchResultItemProps) => {
+  const { isLoading, request } = useAxios()
+
+  const FetchEpisodes = async (page: number) => {
+    const response = await request<EpisodeResult>({
+      server: ANIME,
+      endpoint: `/?method=series&session=${session}&page=${page}`,
+      method: 'GET'
+    })
+    if (response) {
+      setPagination(response.total_pages)
+      setBreadcrumbs(title)
+      setEpisodes(response.episodes)
+      setSelectedSeriesID(session)
+    }
+  }
+
+  return (
+    <Card isPressable disableRipple onPress={() => FetchEpisodes(1)} className="m-4 w-72 cursor-pointer hover:border-primary border-1">
+      <CardHeader className="pb-0 pt-2 px-4 flex-col text-left items-start h-32 overflow-hidden">
+        <div className='flex flex-col gap-y-2 my-2'>
+          <span className="text-default-500">{episodes} Episodes</span>
+          <p className="text-tiny uppercase font-bold">{status}</p>
+        </div>
+        <Divider />
+        <h4 className="font-bold text-large line-clamp-2">{title}</h4>
+      </CardHeader>
+      <CardBody className="relative overflow-hidden items-center py-4 gap-y-2 flex flex-col justify-center">
+        <Image
+          isBlurred
+          alt="background"
+          className="object-cover rounded-xl h-[350px]"
+          src={Prox(poster)}
+          width={240}
+        />
+        <div className='flex gap-x-2 mt-4'>
+          <Chip variant='shadow' radius='sm' color='primary'>{type}</Chip>
+          <Chip variant='shadow' radius='sm' color='secondary'>{year}</Chip>
+          {score && <Chip className='text-white' variant='shadow' radius='sm' color='success'>Score {score}</Chip>}
+        </div>
+        {
+          isLoading && <Spinner className='absolute z-10 top-36' size='lg' />
+        }
+      </CardBody>
+    </Card>
+  )
+}
+
+export default SearchResultItem
